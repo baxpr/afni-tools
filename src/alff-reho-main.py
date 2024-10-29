4d3d87c7ccf36dc1503b1ec63e2fe071ff94948f
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-10-29 17:38:58 -0500

    tester

--- src/alff-reho-main.py
+++ src/alff-reho-main.py
@@ -41,7 +41,7 @@ out = subprocess.run([
     '3dmaskave',
     '-mask', args.mask_niigz,
     'rsfc_ALFF+tlrc.HEAD',
-    ], check=True, stdout=subprocess.PIPE, text=True).stdout
+    ], check=True, stdout=subprocess.PIPE).stdout
 print(out)
 mean_alff = out.split()[0]
 print(f'Mean ALFF: {mean_alff}')
