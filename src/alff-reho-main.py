84f64b776bdca73e6bc13ac44b2072e5668aebaa
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-10-29 17:00:43 -0500

    afnisplit

---
+++ src/alff-reho-main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+#
+# ALFF and REHO via AFNI
+#
+# FIXME Needs QC post container with FSL
+
+import argparse
+import glob
+import os
+import subprocess
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--fmri_niigz', required=True)
+parser.add_argument('--mask_niigz', required=True)
+parser.add_argument('--ort_csv', required=True)
+parser.add_argument('--out_dir', default='/OUTPUTS')
+parser.add_argument('--bphi_hz', default='0.10', type=str)
+args = parser.parse_args()
+
+
+# Work in the output dir to simplify file management
+os.chdir(args.out_dir)
+
+# Run the 3dRSFC command
+# https://github.com/baxpr/afni-alff/blob/6dc80cf/afni-alff.sh
+subprocess.run([
+    '3dRSFC',
+    '-nosat',
+    '-nodetrend',
+    '-no_rs_out',
+    '-ort', args.ort_csv,
+    '-band', '0.00', args.bphi_hz,
+    '-input', args.fmri_niigz,
+    '-mask', args.mask_niigz,
+    '-prefix', 'rsfc',
+    ])
+
+# Normalizing factors
+out = subprocess.run([
+    '3dmaskave',
+    '-mask', args.mask_niigz,
+    'rsfc_ALFF+tlrc.HEAD',
+    ])
+mean_alff = out.stdout.split()[0]
+print(f'Mean ALFF: {mean_alff}')
+
+out = subprocess.run([
+    '3dmaskave',
+    '-mask', args.mask_niigz,
+    'rsfc_fALFF+tlrc.HEAD',
+    ])
+mean_falff = out.stdout.split()[0]
+print(f'Mean fALFF: {mean_falff}')
+
+# Normalize
+subprocess.run([
+    '3dcalc',
+    '-a', 'rsfc_ALFF+tlrc.HEAD',
+    f'-expr "a / {mean_alff}"',
+    '-prefix', 'rsfc_ALFF_norm',
+    ])
+subprocess.run([
+    '3dcalc',
+    '-a', 'rsfc_fALFF+tlrc.HEAD',
+    f'-expr "a / {mean_falff}"',
+    '-prefix', 'rsfc_fALFF_norm',
+    ])
+
+# Convert to nifti
+briks = glob.glob('*.BRIK')
+for brik in briks:
+    subprocess.run(['3dAFNItoNIFTI', brik])
+subprocess.run(['gzip', '*.nii'])
+
--- src/alff-reho-pre.py
+++ src/alff-reho-pre.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 #
-# ALFF and REHO via AFNI
+# Preprocessing for AFNI ALFF/ReHo
 #
-# FIXME Split for pre/main/post containers: python with bids, AFNI, FSL for QC
+#   Find fmri and mask images in fmriprep output and copy to outdir
+#   Find confounds in fmriprep output, select/filter and store in outdir
 
 import argparse
 import bids
-import glob
 import os
 import pandas
-import subprocess
+import shutil
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--fmriprep_dir', required=True)
 parser.add_argument('--out_dir', default='/OUTPUTS')
-parser.add_argument('--bphi_hz', default='0.10', type=str)
 args = parser.parse_args()
-
-
-# Work in the output dir to simplify file management
-os.chdir(args.out_dir)
 
 # BIDS dir info
 bids_fmriprep = bids.layout.BIDSLayout(args.fmriprep_dir, validate=False)
@@ -28,22 +23,26 @@ bids_fmriprep = bids.layout.BIDSLayout(args.fmriprep_dir, validate=False)
 # Find the fmri preprocessed time series and verify there's only one
 fmri_niigz = bids_fmriprep.get(
     extension='.nii.gz',
+    datatype='func',
     desc='preproc',
     suffix='bold',
     )
 if len(fmri_niigz)!=1:
     raise Exception(f'Found {len(fmri_niigz)} fmri .nii.gz instead of 1')
 fmri_niigz = fmri_niigz[0].path
+shutil.copyfile(fmri_niigz, os.path.join(args.out_dir,'fmri.nii.gz'))
 
 # Now the brain mask
 mask_niigz = bids_fmriprep.get(
     extension='.nii.gz',
+    datatype='func',
     desc='brain',
     suffix='mask',
     )
 if len(mask_niigz)!=1:
     raise Exception(f'Found {len(mask_niigz)} mask .nii.gz instead of 1')
 mask_niigz = mask_niigz[0].path
+shutil.copyfile(mask_niigz, os.path.join(args.out_dir,'mask.nii.gz'))
 
 # Find desc-confounds_timeseries.tsv file in fmriprep output and
 # verify there's only one
@@ -80,56 +79,4 @@ keep_cols = keep_cols + [
 # Keep just the ones we want, normalize, and save to file for 3dRSFC -ort option
 conf = conf[keep_cols]
 conf = (conf-conf.mean())/conf.std()
-conf.to_csv('confounds.csv', index=False)
-
-# Run the 3dRSFC command
-# https://github.com/baxpr/afni-alff/blob/6dc80cf/afni-alff.sh
-subprocess.run([
-    '3dRSFC',
-    '-nosat',
-    '-nodetrend',
-    '-no_rs_out',
-    '-ort', 'confounds.csv',
-    '-band', '0.00', args.bphi_hz,
-    '-input', fmri_niigz,
-    '-mask', mask_niigz,
-    '-prefix', 'rsfc',
-    ])
-
-# Normalizing factors
-out = subprocess.run([
-    '3dmaskave',
-    '-mask', mask_niigz,
-    'rsfc_ALFF+tlrc.HEAD',
-    ])
-mean_alff = out.stdout.split()[0]
-print(f'Mean ALFF: {mean_alff}')
-
-out = subprocess.run([
-    '3dmaskave',
-    '-mask', mask_niigz,
-    'rsfc_fALFF+tlrc.HEAD',
-    ])
-mean_falff = out.stdout.split()[0]
-print(f'Mean fALFF: {mean_falff}')
-
-# Normalize
-subprocess.run([
-    '3dcalc',
-    '-a', 'rsfc_ALFF+tlrc.HEAD',
-    f'-expr "a / {mean_alff}"',
-    '-prefix', 'rsfc_ALFF_norm',
-    ])
-subprocess.run([
-    '3dcalc',
-    '-a', 'rsfc_fALFF+tlrc.HEAD',
-    f'-expr "a / {mean_falff}"',
-    '-prefix', 'rsfc_fALFF_norm',
-    ])
-
-# Convert to nifti
-briks = glob.glob('*.BRIK')
-for brik in briks:
-    subprocess.run(['3dAFNItoNIFTI', brik])
-subprocess.run(['gzip', '*.nii'])
-
+conf.to_csv(os.path.join(args.out_dir, 'confounds.csv'), index=False)
