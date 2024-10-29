7f6142b215c89f93fb3781d234ee1b8b4926610c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-10-29 17:08:25 -0500

    tester

--- test_docker.sh
+++ test_docker.sh
@@ -2,12 +2,12 @@
 
 docker run \
     --entrypoint bash \
-    --mount type=bind,src=$(pwd -P)/INPUTS,dst=/INPUTS \
-    --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
+    --mount type=bind,src=$(pwd -P),dst=/wkdir \
     baxterprogers/afni:24.2.06 \
     -c ' \
-    --fmri_niigz /OUTPUTS/fmri.nii.gz \
-    --mask_niigz /OUTPUTS/mask.nii.gz \
+    /wkdir/src/alff-reho-main.py \
+    --fmri_niigz /wkdir/OUTPUTS/fmri.nii.gz \
+    --mask_niigz /wkdir/OUTPUTS/mask.nii.gz \
     --ort_csv /OUTPUTS/confounds.csv \
     --bphi_hz 0.10 \
     '
