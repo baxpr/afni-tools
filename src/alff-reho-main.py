e439f9001fd8e6c6e9c602ca14a9d25ebf3e8a0d
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-10-29 17:26:48 -0500

    tester

--- src/alff-reho-main.py
+++ src/alff-reho-main.py
@@ -42,16 +42,15 @@ out = subprocess.check_output([
     '-mask', args.mask_niigz,
     'rsfc_ALFF+tlrc.HEAD',
     ])
-print(out)
-mean_alff = out.stdout.split()[0]
+mean_alff = out.split()[0]
 print(f'Mean ALFF: {mean_alff}')
 
-out = subprocess.run([
+out = subprocess.check_output([
     '3dmaskave',
     '-mask', args.mask_niigz,
     'rsfc_fALFF+tlrc.HEAD',
-    ], capture_output=True)
-mean_falff = out.stdout.split()[0]
+    ])
+mean_falff = out.split()[0]
 print(f'Mean fALFF: {mean_falff}')
 
 # Normalize
@@ -60,18 +59,17 @@ subprocess.run([
     '-a', 'rsfc_ALFF+tlrc.HEAD',
     f'-expr "a / {mean_alff}"',
     '-prefix', 'rsfc_ALFF_norm',
-    ], capture_output=True)
+    ])
 subprocess.run([
     '3dcalc',
     '-a', 'rsfc_fALFF+tlrc.HEAD',
     f'-expr "a / {mean_falff}"',
     '-prefix', 'rsfc_fALFF_norm',
-    ], capture_output=True)
+    ])
 
 # Convert to nifti
 briks = glob.glob('*.BRIK')
 for brik in briks:
     subprocess.run(['3dAFNItoNIFTI', brik])
-subprocess.run(['gzip', '*.nii'], capture_output=True)
-
+subprocess.run(['gzip', '*.nii'])
 
